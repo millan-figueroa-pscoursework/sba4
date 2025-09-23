1344e6b2e9efc998133e7d9783a43051d8d9bc2b
millan-figueroa <millan.fig@gmail.com>
2025-09-22 19:23:47 -0700

    Adds functionality to render status selection, adds select element to html and updates variables

--- script.js
+++ script.js
@@ -15,18 +15,18 @@ addTaskButton.addEventListener("click", addTask);
 // Create function addTask 1. grabs user input and pushes into taskList, puts everything in an object, clears input field
 
 function addTask() {
-  const id = Date.now();
+  const id = Date.now(); // id for updating task status later
   const input = taskInput.value;
   const category = taskCategory.value;
   const deadline = taskDeadline.value;
-  const status = "in progress"; // default status makes more sense
+  const defaultStatus = "in progress"; // default in-progress status makes more sense
 
   const taskObject = {
     id,
     input,
     category,
     deadline,
-    status,
+    defaultStatus,
   };
 
   taskList.push(taskObject);
@@ -41,7 +41,7 @@ function addTask() {
   console.log(taskObject);
 }
 
-// Create function to render and update task list, use loop  and appendchild?
+// Create function to loop thru taskList and render list items
 
 function renderList() {
   // Clear list instead of slice to fix error and prevent duplicates
@@ -50,13 +50,26 @@ function renderList() {
   // Change loop to for...of bc iterating items not indexes (cleaner)
   for (const task of taskList) {
     // Render name, category, deadline, and separate selector for status
-    const selectStatus = document.createElement("select");
-    // ["in-progress", "complete"]; ??
 
     const li = document.createElement("li");
-    li.textContent = `${task.input} - ${task.category} - ${task.deadline} - ${task.status} `;
+    li.textContent = `${task.input} - ${task.category} - ${task.deadline} - ${task.defaultStatus} `;
 
-    // Render selector for status
+    // Render selector for status and create array with status options
+    const selectStatus = document.createElement("select");
+    const statuses = ["in-progress", "complete", "overdue!"];
+
+    // loop through statuses array
+    for (let i = 0; i < statuses.length; i++) {
+      //loops thru statuses array and creates dropdown options
+      const status = statuses[i];
+      const option = document.createElement("option");
+      // Gets input and assigns to option in storage
+      option.value = status;
+      // Renders to list item
+      option.textContent = status;
+      selectStatus.appendChild(option);
+    }
+
     li.appendChild(selectStatus);
     taskListItem.appendChild(li);
   }
@@ -76,8 +89,9 @@ function loadFromStorage() {
     taskList.push(...JSON.parse(storedTasks));
   }
 }
+
+console.log(localStorage.getItem("todo.tasks"));
 // TODO:
 // Filter by status or category
 // If date is in the past, render date in red and overdue
 // Update displayed task list on status changes
-// Persisting Task Data with Local Storage
